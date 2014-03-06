a82228b881f28ecda7c2a8151d6fa16d56263b60
Kwaye Kant <gabrielkwaye@gmail.com>
2014-03-07 00:15:27 +0100

    no message

---
+++ application/libraries/Ksoft_Auth.php
@@ -0,0 +1,70 @@
+<?php 
+
+    if ( ! defined('BASEPATH')) 
+        exit('No direct script access allowed'); 
+    
+define('ISACTIVATED', 1);
+define('ISNOTACTIVATED', 0);
+
+    class Ksoft_Auth {
+        
+        private  $errors = array();
+        
+        function __construct() {
+            $this->ci_global = & get_instance();
+        }
+        
+        function login($array) {
+            
+//            if($this->ci_global->users_model->is_login_with_username($array['login_username'])){
+//                $login_nature = 'login_with_username';
+//                $login_value = $array['username'];
+//            }
+//            if($this->ci_global->users_model->is_login_with_email($array['login_email'])) {
+//                $login_nature = 'login_with_email';
+//                $login_value = $array['email'];
+//            }
+            
+            if(!is_null($user_info = $this->ci_global->users_model->get_user_info($array))){
+                
+                if($user_info->isactive === ISNOTACTIVATED) {   //User account not activated
+//                    $this->errors = array("user_status" => "not_activated");
+                    $this->ci_global->session->set_userdata("user_not_activated");
+                }else {
+                    if($this->isPasswordMaches($array['password'], $user_info->password)) { // If success
+                        $userSession = array(
+                            "user_id" => $user_info->id,
+                            "user_username" => $user_info->username
+                        );
+                        $this->ci_global->session->set_userdata($userSession);
+                        return TRUE;
+                    } else {
+//                        $this->errors = array("compare_password" => "pwd_not_match");
+                        $this->ci_global->session->set_userdata("pwd_not_match");
+                    }
+                }
+            }
+            return FALSE;
+        }
+        
+        function logout() {
+            $this->ci_global->session->unset_userdata(array('user_id'=>'','user_username'=>'','user_not_activated'=>'', 'pwd_not_match'=>''));
+            $this->ci_global->session->sess_destry();
+        }
+        
+        function isPasswordMaches($incoming_password, $db_password) {
+//            $incoming_password = md5($incoming_password);
+            if($incoming_password === $db_password) return TRUE;
+            return FALSE;
+        }
+        
+        function isLogged() {
+            return $this->ci_global->session->userdata('islogged');
+        }
+        
+        function getDisplayErrors() {
+            return $this->errors;
+        }
+    }
+ 
+?>
